96f45c76f35d33f441486658d8d6d3414d9e00f1
Radu Gasler <miezuit@gmail.com>
2021-03-12 21:00:31 +0200

    curs 5

--- light-and-dark/site.js
+++ light-and-dark/site.js
@@ -8,8 +8,8 @@ function setTheme(theme) {
     let css = document.querySelector("#darkcss")
     css.disabled = theme == "light" ? true : false
 
-    let symbol = css.disabled ? "&#9789;" : "&#9788;"
-    document.querySelector("#switch").innerHTML = symbol
+    // let symbol = css.disabled ? "&#9789;" : "&#9788;"
+    // document.querySelector("#switch").innerHTML = symbol
 
     localStorage.setItem("theme", theme)
 }
